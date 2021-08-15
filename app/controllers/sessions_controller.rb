45af3f5c45b1e2ae4065f7649d31743e35553873
Ismael Cesar <leamscesar@gmail.com>
2021-08-15 14:00:29 -0300

    Polishing the way the login is made No ramo v1.dev Mudanças a serem submetidas: -	modified:   app/controllers/sessions_controller.rb -	modified:   app/javascript/messaging/contacts/index.js -	modified:   app/javascript/presentation/user/login.js -	modified:   app/views/layouts/messaging.html.erb -	modified:   app/views/messaging/contacts/index.html.erb -	modified:   app/views/presentation/user/login.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: user_params[:email])
-    if !@user.nil? || @user.authenticate(user_params[:password])
+    if !@user.nil? && @user.authenticate(user_params[:password])
       user_login(@user)
       redirect_to messaging_contacts_index_path
     else
+      @error_message = 'Invalid user or password'
+      params[:controller] = 'presentation/user'
       render 'presentation/user/login'
     end
   end
--- app/javascript/presentation/user/login.js
+++ app/javascript/presentation/user/login.js
@@ -4,8 +4,20 @@
  * this file contains frontend logic for the login page
  */
 
-  let sign_up = document.querySelector('#btn-sign-up') 
+  const sign_up = document.querySelector('#btn-sign-up') 
   sign_up?.addEventListener('click', (event) =>{
     event.preventDefault();
-    window.location.replace('/presentation/user/sign_up')
-  })
+    window.location.replace('/presentation/user/sign_up');
+  });
+
+  const form_login = document.querySelector('.form-login-signup');
+  form_login?.addEventListener('submit', (event)=>{
+    event.preventDefault(); 
+    data = new FormData(form_login);
+    $.ajax({
+      method: 'POST', 
+      url: '/login', 
+      data: Object.fromEntries(data.entries())
+    });
+    //return false;
+  });
