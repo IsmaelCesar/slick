a992603b5bf00d6033bbfe0abc67ab41025e6507
Ismael Cesar <leamscesar@gmail.com>
2021-10-17 13:50:04 -0300

    Deleting text threads

--- app/models/text_channel.rb
+++ app/models/text_channel.rb
@@ -1,5 +1,5 @@
 class TextChannel < ApplicationRecord
   belongs_to :group
   has_many :text_channel_messages, dependent: :delete_all
-  has_many :text_threads
+  has_many :text_threads, dependent: :destroy
 end
